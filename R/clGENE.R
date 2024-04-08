23c24c346b7e99cbac59eb6c2f09178369efc2e8
lizheng199729 <138444869+lizheng199729@users.noreply.github.com>
2024-04-08 12:55:15 +0800

    fix BUG

--- R/clGENE.R
+++ R/clGENE.R
@@ -1,7 +1,6 @@
-#' Cell-Level Gene Expression Normalization and PCA Analysis
-#'
-#' This function accepts a variable number of gene expression matrices and performs 
-#' a series of analyses including normalization, PCA, and plotting of PCA results. 
+#'The potential molecular mechanisms behind the similar phenotypes of different diseases were explored
+#' This function accepts a variable number of gene expression matrices and performs
+#' a series of analyses including normalization, PCA, and plotting of PCA results.
 #' Optionally, it can identify and export closely related genes based on Euclidean distance.
 #'
 #' @param ... Variable argument list, each representing a gene expression matrix.
@@ -18,7 +17,7 @@ select_top_n<-function(scores,n_top){
   d <- data.frame(
     x   = data.table::copy(scores),
     indice=seq(1,length(scores)))
-  
+
   data.table::setDT(d)
   data.table::setorder(d,-x)
   n_top_indice<-d$indice[1:n_top]
@@ -44,10 +43,10 @@ ssGED <- function(data, group_info, penalty_factor = 0.1) {
   n_sample <- ncol(normalized_data)
   n_gene <- nrow(normalized_data)
   gene_name <- rownames(genexcell)
-  
+
   groups_order <- sort(unique(group_info$group))
   n_cluster <- length(groups_order)
-  
+
   cluster_mat <- matrix(0, nrow = n_cluster, ncol = n_sample)
   order_i <- 1
   for (group_i in groups_order) {
@@ -55,13 +54,13 @@ ssGED <- function(data, group_info, penalty_factor = 0.1) {
     cluster_mat[order_i, idx_i] <- 1
     order_i <- order_i + 1
   }
-  
+
   genexcell <- Matrix(as.matrix(genexcell), sparse = TRUE)
   cosine_sim <- proxyC::simil(genexcell, cluster_mat, method = "cosine", drop0 = TRUE)
-  
+
   # Apply L1 penalty to cosine similarity
   penalized_cosine_sim <- apply(cosine_sim, 2, function(column) penalty_function_L1(column, penalty_factor))
-  
+
   # Initializes the resulting data frame
   rank_stats_names <- data.frame(matrix(NA, n_gene, length(groups_order),
                                         dimnames = list(seq(1, n_gene), groups_order)),
@@ -69,27 +68,27 @@ ssGED <- function(data, group_info, penalty_factor = 0.1) {
   rank_stats_scores <- data.frame(matrix(NA, n_gene, length(groups_order),
                                          dimnames = list(seq(1, n_gene), groups_order)),
                                   stringsAsFactors = FALSE)
-  
+
   order_i <- 1
   for (group_i in groups_order) {
     idx_i <- group_info == group_i
     scores <- penalized_cosine_sim[, order_i]
-    
+
     # The select_top_n function was used for scoring
     global_indices <- select_top_n(scores, n_gene)
     rank_stats_names[, order_i] <- gene_name[global_indices]
     rank_stats_scores[, order_i] <- scores[global_indices]
-    
+
     order_i <- order_i + 1
   }
-  
+
   colnames(rank_stats_names) <- groups_order
   colnames(rank_stats_scores) <- groups_order
-  
+
   ranks_stats <- list(names = rank_stats_names,
                       scores = rank_stats_scores)
-  
-  
+
+
   first_5 <- apply(ranks_stats$name, 2, head, n = 20)
   merged_column <- c(first_5)
   TOPexp<- ncol(normalized_data)
@@ -100,7 +99,7 @@ ssGED <- function(data, group_info, penalty_factor = 0.1) {
 # Define a function that transforms the input matrix into a new S4 object
 clGENE <- function(...){
   matrices <- list(...)
-  
+
   # Initializes the result list
   results <- list()
   grouped_pca_results <- list()
@@ -121,22 +120,22 @@ clGENE <- function(...){
     TOPexp<- ncol(normalized_data)
     TOPgene <-normalized_data[merged_column, (1:TOPexp)]
     n_rows <- nrow(TOPgene)
-    
+
     # Create a sequence representing the starting row number of each submatrix
     row_indices <- seq(1, n_rows, 20)
-    
+
     # The matrix is split using the lapply and split functions
     list_of_matrices <- lapply(row_indices, function(i) {
       # Calculates the end row number of the submatrix
       end_row <- min(i + 19, n_rows)
-      
+
       # Extract the submatrix and return it
       TOPgene[i:end_row, ]
     })
     list_of_pca_results <- lapply(list_of_matrices, function(sub_matrix) {
       # Transpose the matrix so that the rows become columns
       transposed_matrix <- t(sub_matrix)
-      
+
       # The transposed matrix was checked to see if it contained sufficient data for PCA
       # At least two rows, two columns of the original matrix, are required to perform PCA
       if(nrow(transposed_matrix) > 1) {
@@ -150,66 +149,66 @@ clGENE <- function(...){
     })
     results[[i]] <- list_of_pca_results
     grouped_pca_results <- list()
-    
+
     for(i in seq_along(results)) {
       pca_results <- results[[i]]
-      
+
       for(pca_result in pca_results) {
         if(!is.null(pca_result)) {
           # Extract the name of the current PCA result
           name <- rownames(pca_result$x)[1]
-          
+
           # If the matrix of that name is not already grouped, initialize the grouping
           if(!name %in% names(grouped_pca_results)) {
             grouped_pca_results[[name]] <- list()
           }
-          
+
           # The current PCA result is added to the corresponding grouping
           grouped_pca_results[[name]] <- c(grouped_pca_results[[name]], list(pca_result))
         }
       }
     }
-    
+
     # Mapping analysis was performed for each group
     for(name in names(grouped_pca_results)) {
       pca_group <- grouped_pca_results[[name]]
-      
+
       # Create a blank artboard to draw multiple PCA results
       plot(NULL, xlim = c(-1, 1), ylim = c(-1, 1), xlab = "PC1", ylab = "PC2", main = paste("PCA for", name))
-      
+
       for(pca_result in pca_group) {
         # Scatter plots were drawn for each PCA result
         points(pca_result$x[,1], pca_result$x[,2], col = rainbow(length(pca_result$x[,1])), pch = 16, cex = 0.6)
-        
+
         # Add labels so that you know which list each point comes from
         text(pca_result$x[,1], pca_result$x[,2], labels = seq_along(pca_result$x[,1]), pos = 3, cex = 0.5)
       }
     }
     for(name in names(grouped_pca_results)) {
       pca_group <- grouped_pca_results[[name]]
-      
+
       # Create a blank artboard to draw multiple PCA results
       plot(NULL, xlim = c(-1, 1), ylim = c(-1, 1), xlab = "PC1", ylab = "PC2", main = paste("PCA for", name))
-      
+
       for(pca_result in pca_group) {
         # Scatter plots were drawn for each PCA result
         points(pca_result$x[,1], pca_result$x[,2], col = rainbow(length(pca_result$x[,1])), pch = 16, cex = 0.6)
         # Add labels so that you know which list each point comes from
         text(pca_result$x[,1], pca_result$x[,2], labels = seq_along(pca_result$x[,1]), pos = 3, cex = 0.5)
       }
-      
+
       # If needed to export close gene pairs
       if(export_close_genes) {
         # Coordinates and gene names were extracted
         coords <- pca_result$x[, 1:2] # Suppose we consider only the first two principal components
         genes <- rownames(coords)
-        
+
         # Compute the Euclidean distance between all pairs of points
         dist_matrix <- as.matrix(dist(coords))
-        
+
         # Initializes a data frame to store pairs of genes that are close together
         close_genes <- data.frame(gene1 = character(), gene2 = character(), distance = numeric())
-        
+
         # The distance matrix was traversed to find close gene pairs
         for(j in 1:(nrow(dist_matrix)-1)) {
           for(k in (j+1):nrow(dist_matrix)) {
@@ -218,13 +217,13 @@ clGENE <- function(...){
             }
           }
         }
-        
+
         # Close gene pairs are added to the list
         close_genes_list[[paste(name, i)]] <- close_genes
       }
     }
   }
-  
+
   # If needed to export close gene pairs
   if(export_close_genes) {
     # Close gene pairs for each grouping were derived
@@ -232,7 +231,7 @@ clGENE <- function(...){
       write.csv(close_genes_list[[name]], sprintf("%s_close_genes.csv", name), row.names = FALSE)
     }
   }
-  
+
   return(results)
 }
 
